49668b1d2b1f44f94253a8a57058f8f0fb547c64
delongchen <fg456@vip.qq.com>
2020-05-09 02:39:53 +0800

    init

---
+++ src/main/java/pub/cdl/maogay/MaogayApplication.java
@@ -0,0 +1,13 @@
+package pub.cdl.maogay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaogayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MaogayApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pub/cdl/maogay/MaogayApplicationTests.java
@@ -0,0 +1,13 @@
+package pub.cdl.maogay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MaogayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
